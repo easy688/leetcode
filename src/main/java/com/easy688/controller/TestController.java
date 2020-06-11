deb8fa8593c2f3ed12e9d7b3b8de9fcab4f1e425
easy688 <850530237@qq.com>
2020-06-12 00:18:13 +0800

    jenkins测试3

--- src/main/java/com/easy688/controller/TestController.java
+++ src/main/java/com/easy688/controller/TestController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
     private Integer num=0;
+    private String version="1";
     @GetMapping("/test")
     @ResponseBody
     public String test(){
         num++;
-        return "success"+num;
+        return "success"+num+version;
     }
 }
